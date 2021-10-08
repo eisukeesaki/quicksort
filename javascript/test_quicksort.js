13fd288bcde62e2fc32d892fe7c467037ab282de
eisuke <izkyyy@gmail.com>
2021-10-07 22:46:49 -0700

    added new test case
    that tests quicksort with an array with ${nElms} numbers, that's pre-sorted in descending order

--- javascript/test_quicksort.js
+++ javascript/test_quicksort.js
@@ -14,34 +14,67 @@ const genArrOfRandNums = (nElms, maxNum) => {
   return arr;
 }
 
-const testQuicksort = () => {
-  const nElms = 1000000;
+const testQuicksortRand = () => {
+  const nElms = 10000;
   const maxNum = Number.MAX_SAFE_INTEGER;
+
   let toSort = genArrOfRandNums(nElms, maxNum); // TODO: figure out why the test works with `const toSort`
+
   let timeStartSort = null;
   let timeSortComplete = null;
+
   let i = 0;
 
-  // ------------------------------------+-----------------------  hard-coded test arrays
-  // let toSort = [10, 90, 9, -12, -4, 50, 33, -45, 3, -5];
-  // let toSort = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4];
-  // let toSort = [-1, 5, 2, 7, -8, 5, 4];
-  // let toSort = [42];
-  // ------------------------------------+----------------------- / hard-coded test arrays
-  // console.log(`calling quicksort(${toSort});`);
+  console.log(`>running quicksort on an array with ${nElms} random numbers...`);
   timeStartSort = Date.now();
   quicksort(toSort, 0, toSort.length - 1);
   timeSortComplete = Date.now();
    
-    while (i < toSort.length - 1 && toSort[i] < toSort[i + 1]) {
-      i++;
-    }
-    if (i === nElms - 1) {
-      console.log("OK.");
-      console.log(`Execution time: ${timeSortComplete - timeStartSort}ms`); // TODO: measure performance more accurately
+  while (i < toSort.length - 1 && toSort[i] < toSort[i + 1]) {
+    i++;
+  }
+
+  if (i === nElms - 1) {
+    console.log("OK.");
+    console.log(`took ${timeSortComplete - timeStartSort}ms\n`); // TODO: measure performance more accurately
   } else {
-    console.log("KO.");
+    console.log("KO.\n");
   }
 }
 
-testQuicksort();
+const testQuicksortRev = () => {
+  const nElms = 10000;
+  const maxNum = Number.MAX_SAFE_INTEGER;
+
+  let toSort = []; // TODO: figure out why the test works with `const toSort`
+
+  let timeStartSort = null;
+  let timeSortComplete = null;
+
+  let i = 0;
+  let m = 0;
+  
+  for (let k = maxNum; m < nElms; k--) {
+    toSort.push(k);
+    m++;
+  }
+  
+  console.log(`>running quicksort on an array with ${nElms} numbers, pre-sorted in descending order...`);
+  timeStartSort = Date.now();
+  quicksort(toSort, 0, toSort.length - 1);
+  timeSortComplete = Date.now();
+
+  while (i < toSort.length - 1 && toSort[i] < toSort[i + 1]) {
+    i++;
+  }
+
+  if (i === nElms - 1) {
+    console.log("OK.");
+    console.log(`took ${timeSortComplete - timeStartSort}ms\n`); // TODO: measure performance more accurately
+  } else {
+    console.log("KO.\n");
+  }
+}
+
+testQuicksortRand();
+testQuicksortRev();
